c9151b19b71e7a4c26e958c61d4e8da2524502d4
abien.agarap <abien.agarap@senti.com.ph>
2019-07-06 18:42:59 +0800

    chore : add config 1 map

--- level_zero.py
+++ level_zero.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 __version__ = '1.0.0'
 __author__ = 'Abien Fred Agarap'
 
+import os
 import random
 
 random.seed(42)
@@ -16,22 +17,62 @@ miner = 'v'
 
 miner_row, miner_col = 0, 0
 
-while True:
-    pit_row = random.randint(1, 7)
-    pit_col = random.randint(1, 7)
-    gold_row = random.randint(1, 7)
-    gold_col = random.randint(1, 7)
-    beacon_row = random.randint(1, 7)
-    beacon_col = random.randint(1, 7)
-    if pit_row != gold_row != beacon_row and pit_col != gold_col != beacon_col:
-        break
+# config 1
 
-grid = [['_' for _ in range(8)] for _ in range(8)]
-grid[miner_row][miner_col] = miner
-grid[pit_row][pit_col] = pit
-grid[gold_row][gold_col] = gold
-grid[beacon_row][beacon_col] = beacon
+# pit_xy = [[0, 3], [2, 2], [2, 6], [5, 2]]
 
+# pit_row_0 = 0
+# pit_col_0 = 3
+#
+# pit_row_1 = 2
+# pit_col_1 = 2
+#
+# pit_row_2 = 2
+# pit_col_2 = 6
+#
+# pit_row_3 = 5
+# pit_col_3 = 2
+
+# beacon_xy = [[4, 0], [0, 4]]
+
+# beacon_row_0 = 4
+# beacon_col_0 = 0
+
+# beacon_row_1 = 0
+# beacon_col_1 = 4
+gold_row, gold_col = 4, 4
+# config 1
+
+# config 2
+pit_xy = [[21, 0], [21, 1], [20, 2]]
+beacon_xy = [[22, 0], [31, 1]]
+gold_row, gold_col = 22, 1
+# config 2
+
+# while True:
+#     pit_row = random.randint(1, 7)
+#     pit_col = random.randint(1, 7)
+#     gold_row = random.randint(1, 7)
+#     gold_col = random.randint(1, 7)
+#     beacon_row = random.randint(1, 7)
+#     beacon_col = random.randint(1, 7)
+#     if pit_row != gold_row != beacon_row and pit_col != gold_col != beacon_col:
+#         break
+
+def setup_environment():
+
+    grid = [['_' for _ in range(32)] for _ in range(32)]
+    grid[miner_row][miner_col] = miner
+
+    for coordinates in pit_xy:
+        grid[coordinates[0]][coordinates[1]] = pit
+
+    grid[gold_row][gold_col] = gold
+
+    for coordinates in beacon_xy:
+        grid[coordinates[0]][coordinates[1]] = beacon
+
+    return grid
 
 def print_grid(new_miner_row, new_miner_col):
     grid[new_miner_row][new_miner_col] = miner
@@ -39,9 +80,12 @@ def print_grid(new_miner_row, new_miner_col):
     for row in grid:
         print('{}'.format(row))
 
-while True:
+success = False
+move = 0
 
-    print('miner\t:\t[{}][{}]\ngold\t:\t[{}][{}]\npit\t:\t[{}][{}]'.format(miner_row, miner_col, gold_row, gold_col, pit_row, pit_col))
+while success == False:
+
+    grid = setup_environment()
 
     rotate = random.randint(1, 4)
 
@@ -54,29 +98,45 @@ while True:
     elif rotate == 4:
         miner = '^'
 
+    move += 1
+
     old_miner_row, old_miner_col = miner_row, miner_col
 
     if miner == 'v':
         if miner_row >= 0 and miner_row < 7:
             old_miner_row = miner_row
             miner_row += 1
+            move += 1
     elif miner == '>':
         if miner_col >= 0 and miner_col < 7:
             old_miner_col = miner_col
             miner_col += 1
+            move += 1
     elif miner == '<':
         if miner_col > 0 and miner_col > -1:
             old_miner_col = miner_col
             miner_col -= 1
+            move += 1
     elif miner == '^':
         if miner_row > 0 and miner_row > -1:
             old_miner_row = miner_row
             miner_row -= 1
+            move += 1
 
     grid[old_miner_row][old_miner_col] = '-'
+    print('miner\t:\t[{}][{}]\ngold\t:\t[{}][{}]'.format(miner_row, miner_col, gold_row, gold_col))
     print_grid(miner_row, miner_col)
     print()
 
     if (miner_row == gold_row) and (miner_col == gold_col):
-        print('success')
-        break
+        print('success in {} moves'.format(move))
+        success = True
+    elif [miner_row, miner_col] in pit_xy:
+        os.system('clear')
+        print('reset')
+        move = 0
+        miner = 'v'
+        grid = setup_environment()
+        miner_row, miner_col = 0, 0
+        grid[miner_row][miner_col] = miner
+
