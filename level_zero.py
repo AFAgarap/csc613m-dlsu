98f1c921e58e2fdf2448e0fb9373fca5ceaef51e
Abien Fred Agarap <abien_agarap@dlsu.edu.ph>
2019-07-06 19:29:49 +0800

    chore : add code cleanup

--- level_zero.py
+++ level_zero.py
@@ -19,30 +19,12 @@ beacon = 'B'
 miner = 'v'
 
 miner_row, miner_col = 0, 0
+success = False
+move = 0
 
 # config 1
-
 pit_xy = [[0, 3], [2, 2], [2, 6], [5, 2]]
-
-pit_row_0 = 0
-pit_col_0 = 3
-
-pit_row_1 = 2
-pit_col_1 = 2
-
-pit_row_2 = 2
-pit_col_2 = 6
-
-pit_row_3 = 5
-pit_col_3 = 2
-
 beacon_xy = [[4, 0], [0, 4]]
-
-beacon_row_0 = 4
-beacon_col_0 = 0
-
-beacon_row_1 = 0
-beacon_col_1 = 4
 gold_row, gold_col = 4, 4
 # config 1
 
@@ -52,44 +34,35 @@ gold_row, gold_col = 4, 4
 #gold_row, gold_col = 22, 1
 # config 2
 
-# while True:
-#     pit_row = random.randint(1, 7)
-#     pit_col = random.randint(1, 7)
-#     gold_row = random.randint(1, 7)
-#     gold_col = random.randint(1, 7)
-#     beacon_row = random.randint(1, 7)
-#     beacon_col = random.randint(1, 7)
-#     if pit_row != gold_row != beacon_row and pit_col != gold_col != beacon_col:
-#         break
+def setup_environment(cells=8):
 
-def setup_environment():
-    grid = [['*' for _ in range(8)] for _ in range(9)]
-#    grid = [['_' for _ in range(32)] for _ in range(32)]
-    grid[miner_row][miner_col] = miner
+    grid = [['*' for _ in range(cells)] for _ in range(cells)]
 
     for coordinates in pit_xy:
         grid[coordinates[0]][coordinates[1]] = pit
 
-    grid[gold_row][gold_col] = gold
-
     for coordinates in beacon_xy:
         grid[coordinates[0]][coordinates[1]] = beacon
 
+    grid[gold_row][gold_col] = gold
+    grid[miner_row][miner_col] = miner
+
     return grid
+
 
 def print_grid(new_miner_row, new_miner_col):
     grid[new_miner_row][new_miner_col] = miner
     os.system('figlet -c -w {} Miner'.format(columns))
+    print('CSC613M Introduction to Intelligent Systems'.center(columns))
+    print('DLSU 3rd Trimester A.Y. 2018-2019'.center(columns))
+    print()
     for row in grid:
         row_text = ''
         for cell in row:
             row_text += cell + ' '
         print(row_text.center(columns), end='')
         print()
-#        print('{}'.format(row).center(columns))
 
-success = False
-move = 0
 
 while success == False:
 
@@ -110,26 +83,22 @@ while success == False:
 
     old_miner_row, old_miner_col = miner_row, miner_col
 
-    if miner == 'v':
-        if miner_row >= 0 and miner_row < 7:
-            old_miner_row = miner_row
-            miner_row += 1
-            move += 1
-    elif miner == '>':
-        if miner_col >= 0 and miner_col < 7:
-            old_miner_col = miner_col
-            miner_col += 1
-            move += 1
-    elif miner == '<':
-        if miner_col > 0 and miner_col > -1:
-            old_miner_col = miner_col
-            miner_col -= 1
-            move += 1
-    elif miner == '^':
-        if miner_row > 0 and miner_row > -1:
-            old_miner_row = miner_row
-            miner_row -= 1
-            move += 1
+    if miner == 'v' and miner_row >= 0 and miner_row < 7:
+        old_miner_row = miner_row
+        miner_row += 1
+        move += 1
+    elif miner == '>' and miner_col >= 0 and miner_col < 7:
+        old_miner_col = miner_col
+        miner_col += 1
+        move += 1
+    elif miner == '<' and miner_col > 0 and miner_col > -1:
+        old_miner_col = miner_col
+        miner_col -= 1
+        move += 1
+    elif miner == '^' and miner_row > 0 and miner_row > -1:
+        old_miner_row = miner_row
+        miner_row -= 1
+        move += 1
 
     time.sleep(0.5)
 
