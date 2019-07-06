b44228a92d98f8d689cf6ab3fb80064fc39e0c88
Abien Fred Agarap <abien_agarap@dlsu.edu.ph>
2019-07-06 19:53:17 +0800

    chore : add markers module

---
+++ markers.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+__version__ = '1.0.0'
+__author__ = 'Abien Fred Agarap'
+
+
+FACE_EAST = '>'
+FACE_NORTH = '^'
+FACE_SOUTH = 'v'
+FACE_WEST = '<'
+
+BEACON = 'B'
+GOLD = 'G'
+MINER = FACE_SOUTH
+PIT = 'P'
