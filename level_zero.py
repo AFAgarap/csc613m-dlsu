203298d4d1ed3627a48423755a1ac9c52cf1a917
Abien Fred Agarap <abien_agarap@dlsu.edu.ph>
2019-07-06 20:01:01 +0800

    feat : remove hard-coded configuration

--- level_zero.py
+++ level_zero.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 __version__ = '1.0.0'
 __author__ = 'Abien Fred Agarap'
 
+from config import use_config_1
 from markers import *
 import os
 import random
@@ -18,17 +19,8 @@ miner_row, miner_col = 0, 0
 success = False
 move = 0
 
-# config 1
-pit_xy = [[0, 3], [2, 2], [2, 6], [5, 2]]
-beacon_xy = [[4, 0], [0, 4]]
-gold_row, gold_col = 4, 4
-# config 1
+pit_xy, beacon_xy, gold_row, gold_col = use_config_1()
 
-# config 2
-# pit_xy = [[21, 0], [21, 1], [20, 2]]
-# beacon_xy = [[22, 0], [31, 1]]
-# gold_row, gold_col = 22, 1
-# config 2
 
 def setup_environment(cells=8):
 
