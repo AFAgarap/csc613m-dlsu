8c43d8fc8c3e5a492f233b60780a7a8d596f45ac
Abien Fred Agarap <abien_agarap@dlsu.edu.ph>
2019-07-13 23:25:44 +0800

    chore : add agents

---
+++ mco2/level_0/agents.py
@@ -0,0 +1,39 @@
+from environment import after_action_state
+from environment import check_game_status
+from environment import tomark
+import random
+
+
+class BaseAgent(object):
+    def __init__(self, mark):
+        self.mark = mark
+
+    def act(self, state, ava_actions):
+        for action in ava_actions:
+            nstate = after_action_state(state, action)
+            gstatus = check_game_status(nstate[0])
+            if gstatus > 0:
+                if tomark(gstatus) == self.mark:
+                    return action
+        return random.choice(ava_actions)
+
+
+class HumanAgent(object):
+    def __init__(self, mark):
+        self.mark = mark
+
+    def act(self, ava_actions):
+        while True:
+            uloc = input("Enter location[1-9], q for quit: ")
+            if uloc.lower() == 'q':
+                return None
+            try:
+                action = int(uloc) - 1
+                if action not in ava_actions:
+                    raise ValueError()
+            except ValueError:
+                print("Illegal location: '{}'".format(uloc))
+            else:
+                break
+
+        return action
