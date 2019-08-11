f834924852f7a559e9842d9c643e41499a2db587
Abien Fred Agarap <abien_agarap@dlsu.edu.ph>
2019-08-11 21:27:45 +0800

    chore : add more kinship plays

--- mco3/tutorial.pl
+++ mco3/tutorial.pl
@@ -57,3 +57,34 @@ parent(bob, charlie).
 
 % we can check the grandchildren of someone by
 % parent(albert, X), parent(X, Y).
+
+get_grandchild :-
+    parent(albert, X), parent(X, Y),
+    write('Albert\'s grandchild is '),
+    write(Y), nl.
+
+% let us check if two people are siblings
+% parent(X, carl), parent(X, charlie).
+
+% use tilde for variable
+% ~n is nl
+get_grandparent :-
+    parent(X, carl),
+    parent(X, charlie),
+    format('~w ~s grandparent ~n', [X, "is the"]).
+
+brother(bob, bill).
+
+% check if carl has an uncle
+% parent(X, carl), brother(X, Y).
+
+% check if carl has a sibling
+% parent(X, carl), parent(X, Y).
+
+% display the grand parent of someone
+% this will not conflict with the get_grandparent above
+% since this accepts atoms while the one above does not
+get_grandparent(X, Y) :-
+    parent(Z, X),
+    parent(Y, Z),
+    format('the grandparent of ~w is ~w ~n', [X, Y]).
